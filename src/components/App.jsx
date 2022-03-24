ebde3ad943fb6e0bec09d01df0685c8bbb2adc97
estebansant <estebansant30@gmail.com>
2022-03-24 15:55:20 -0300

    sending the website to deploy with Netlify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     return(
         <React.Fragment>
             <h1 className="title">Hello World! (With React)</h1>
-            <p className="text">These are my first lines of code with React while using Webpack and Babel!</p>
+            <p className="text">Here are my first lines of code with React while using Webpack and Babel!</p>
         </React.Fragment>
     )
     
